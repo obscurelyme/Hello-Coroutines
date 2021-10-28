f11f22fe5c301bbd156b052f2765f5e2b3774348
obscurelyme <nico.frozencoffee@gmail.com>
2021-10-28 12:26:03 -0700

    coroutine with class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <boost/coroutine2/all.hpp>
+#include <functional>
 
 template<typename T>
 using coroutine = boost::coroutines2::coroutine<T>;
@@ -15,15 +16,69 @@ void myCoroutine(coroutine<void>::push_type &yield) {
     std::cout << "coroutine: Everyone" << std::endl;
 }
 
+int intCo(coroutine<int>::push_type &yield) {
+    static int myInt = 0;
+    while (true) {
+        yield(++myInt);
+    }
+}
+
+class MyClass {
+    public:
+    MyClass() {
+        pull = new coroutine<int>::pull_type(std::bind(&MyClass::EnumerateEnemies, this, std::placeholders::_1));
+    }
+    ~MyClass() { delete pull; };
+
+    void Update() {
+        // if (_currentEnemySpawn < _maxEnemies) {
+        //     (*pull)();
+        // }
+        try { 
+            (*pull)();
+        } catch (const std::exception& e) {
+            std::cout << e.what() << std::endl;
+        }
+        
+    }
+
+    void EnumerateEnemies(coroutine<int>::push_type &yield) {
+        // while (_currentEnemySpawn < _maxEnemies) {
+        //     // spawn enemy...
+        //     yield(_currentEnemySpawn++);
+        // }
+        yield(++_currentEnemySpawn);
+    }
+
+    void ResetEnumeration() {
+        _currentEnemySpawn = 0;
+    }
+
+    int CurrentEnemy() const { return pull->get(); }
+
+    private:
+    coroutine<int>::pull_type* pull;
+    int _currentEnemySpawn{0};
+    int _maxEnemies{6};
+};
+
 int main(int, char**) {
-    coroutine<void>::pull_type resume{myCoroutine};
-    std::cout << "[main]: ....... " << std::endl; //flush here
-    resume();
-    std::cout << "[main]: here at NETWAYS! :)" << std::endl;
-    std::cout << "[main]: here at NETWAYS! :)" << std::endl;
-    std::cout << "[main]: here at NETWAYS! :)" << std::endl;
-    std::cout << "[main]: here at NETWAYS! :)" << std::endl;
-    std::cout << "[main]: here at NETWAYS! :)" << std::endl;
-    resume();
-    resume();
+    MyClass cl;
+    std::cout << cl.CurrentEnemy() << std::endl;
+    cl.Update();
+    std::cout << cl.CurrentEnemy() << std::endl;
+    cl.Update();
+    std::cout << cl.CurrentEnemy() << std::endl;
+    cl.Update();
+    std::cout << cl.CurrentEnemy() << std::endl;
+    cl.Update();
+    std::cout << cl.CurrentEnemy() << std::endl;
+    cl.Update();
+    std::cout << cl.CurrentEnemy() << std::endl;
+    cl.Update();
+    std::cout << cl.CurrentEnemy() << std::endl;
+    cl.Update();
+    std::cout << cl.CurrentEnemy() << std::endl;
+    cl.Update();
+    std::cout << cl.CurrentEnemy() << std::endl;
 }
